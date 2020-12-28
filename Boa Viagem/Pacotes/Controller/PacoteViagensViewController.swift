9258cecbdc055617750cfffc4a249fd614c82ecc
Lleitao1 <lucas.leitao25@gmail.com>
2020-12-28 17:43:10 -0300

    criar e formatar view de pacotes

---
+++ Boa Viagem/Pacotes/Controller/PacoteViagensViewController.swift
@@ -0,0 +1,54 @@
+//
+//  PacoteViagensViewController.swift
+//  Boa Viagem
+//
+//  Created by Lucas Abdel Leitao on 28/12/20.
+//  Copyright © 2020 Lucas Abdel Leitao. All rights reserved.
+//
+
+import UIKit
+
+class PacoteViagensViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout{
+    
+    @IBOutlet weak var colecaoPacotesViagem: UICollectionView!
+    
+    let listaViagens: Array<Viagem> = ViagemDAO().retornaTodasAsViagens()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        colecaoPacotesViagem.dataSource = self
+        colecaoPacotesViagem.delegate = self
+    }
+    
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return self.listaViagens.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let celulaPacote = collectionView.dequeueReusableCell(withReuseIdentifier: "celulaPacote", for: indexPath) as! PacoteViagemCollectionViewCell
+        
+        let viagemAtual = listaViagens[indexPath.item]
+        
+        celulaPacote.labelTitulo.text = viagemAtual.titulo
+        celulaPacote.labelQuantidadeDias.text = "\(viagemAtual.quantidadeDeDias) dias"
+        celulaPacote.labelPreco.text = "R$ \(viagemAtual.preco)"
+        celulaPacote.ImagemViagem.image = UIImage(named: viagemAtual.imgpath)
+        
+        celulaPacote.layer.borderWidth = 0.5
+        celulaPacote.layer.borderColor = UIColor(red: 85.0/255.0, green: 85.0/255.0, blue: 85.0/255.0, alpha: 1).cgColor
+        
+        celulaPacote.layer.cornerRadius = 8
+        
+        
+        return celulaPacote
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        let larguraDaCelula = collectionView.bounds.width/2.0
+        return CGSize(width: larguraDaCelula-15, height: 160)
+    }
+
+}
---
+++ Boa Viagem/Pacotes/View/PacoteViagemCollectionViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  PacoteViagemCollectionViewCell.swift
+//  Boa Viagem
+//
+//  Created by Lucas Abdel Leitao on 28/12/20.
+//  Copyright © 2020 Lucas Abdel Leitao. All rights reserved.
+//
+
+import UIKit
+
+class PacoteViagemCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var ImagemViagem: UIImageView!
+    
+    @IBOutlet weak var labelTitulo: UILabel!
+    
+    @IBOutlet weak var labelQuantidadeDias: UILabel!
+    
+    @IBOutlet weak var labelPreco: UILabel!
+    
+    
+    
+}
